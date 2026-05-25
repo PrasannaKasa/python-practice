00ff2bfb2f85e7a51a6bf48ea98f4a80ba718f7b
Kasa Vijaya prasanna <141217348+PrasannaKasa@users.noreply.github.com>
2026-05-25 21:45:43 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,9 @@
+student_name = "prasanna"
+Roll_no = 61
+l = ["python","java","python","sql"]
+subjects = set(l)
+marks = {"Python":60,"java":75,"sql":90}
+print(student_name)
+print(Roll_no)
+print(subjects)
+print(marks)
