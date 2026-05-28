71537845056945fb61c93aae80df8bc65b31d032
Kasa Vijaya prasanna <141217348+PrasannaKasa@users.noreply.github.com>
2026-05-28 18:31:03 +0530

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,14 @@
+x = [1,2,3]
+def function():
+    
+    y =[4,5,6]
+    x.append(100)
+    z= (10,11,12)
+    def add():
+        print(x)
+
+        print(y)
+        print(z)
+    add()
+
+function()
